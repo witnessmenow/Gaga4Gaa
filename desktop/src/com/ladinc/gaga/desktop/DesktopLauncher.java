e4b8829a6c900c3c2613f73cec414bef40b1c46b
Gary O' Donoghue <gary.odonoghue@gmail.com>
2014-06-28 14:30:55 +0100

    initial commit

---
+++ desktop/src/com/ladinc/gaga/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ladinc.gaga.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ladinc.gaga.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}

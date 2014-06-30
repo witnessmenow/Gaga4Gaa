a133f5e1ee983c64cbb31e1667ede664f44bea05
Gary O' Donoghue <gary.odonoghue@gmail.com>
2014-06-30 20:54:26 +0100

    add new game class

---
+++ core/src/main/java/com/ladinc/gaga/core/Gaga.java
@@ -0,0 +1,15 @@
+package com.ladinc.gaga.core;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+
+public class Gaga extends Game
+{	
+	@Override
+	public void create() 
+	{
+		
+		Gdx.app.setLogLevel(Application.LOG_ERROR);		
+	}
+}
